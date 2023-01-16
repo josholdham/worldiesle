f117a27fbe1ba31a882605db781a5ecc0e5e2063
Josh Oldham <oldham.josh@gmail.com>
2023-01-16 17:29:57 +0000

    attempt to add github workflow cron

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -8,19 +8,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log('Revalidating...', req.query.secret);
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  if (req.query.secret !== process.env.WORLDIESLE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate('/path-to-revalidate');
+    await res.revalidate('/');
     return res.json({ message: 'Re-validated' });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send({ message: 'Error revalidating' });
+    console.log('errror');
+    console.log(err);
+    return res.status(500).send({ message: 'Error revalidating!' });
   }
 }
