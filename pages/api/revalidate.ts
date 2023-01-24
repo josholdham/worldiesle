f10616202938637702e233e40d27ca28801691e3
Josh Oldham <oldham.josh@gmail.com>
2023-01-24 11:18:07 +0000

    revalidation endpoint returns date

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   message: string;
+  date: Date;
 };
 
 export default async function handler(
@@ -11,19 +12,23 @@ export default async function handler(
   console.log('Revalidating...', req.query.secret);
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.WORLDIESLE_SECRET) {
-    return res.status(401).json({ message: 'Invalid token' });
+    return res
+      .status(401)
+      .json({ message: 'Invalid token', date: new Date() });
   }
 
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
     await res.revalidate('/');
-    return res.json({ message: 'Re-validated' });
+    return res.json({ message: 'Re-validated', date: new Date() });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
     console.log('errror');
     console.log(err);
-    return res.status(500).send({ message: 'Error revalidating!' });
+    return res
+      .status(500)
+      .send({ message: 'Error revalidating!', date: new Date() });
   }
 }
