5aa7088da17c4ab5bd971982a00d5cd69a6c3382
Josh Oldham <oldham.josh@gmail.com>
2023-04-29 11:03:50 +0100

    add prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  parser: 'typescript',
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: false,
+  bracketSpacing: true,
+  semi: true
 }
+ 
